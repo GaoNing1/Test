99c58e285bbb0799f639c2f6bd70dad22c1182e6
Gncute <841895174@qq.com>
2018-01-17 16:33:30 +0800

    注解式增删改查

---
+++ springMybatis/src/main/java/com/spring/mybatis/springMybatis/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.spring.mybatis.springMybatis.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.mybatis.springMybatis.dao.UserMapper;
+import com.spring.mybatis.springMybatis.entity.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @RequestMapping(value={"/selectUserById"}, method=RequestMethod.GET)
+    public User selectUserById(String id){
+        User user = userMapper.selectUserById(Integer.parseInt(id));
+        return user;
+    }
+
+    @RequestMapping(value={"/selectUserByName"}, method=RequestMethod.GET)
+    public List<User> selectUserByName(String userName){
+        return userMapper.selectUserByName(userName);
+    }
+
+    @RequestMapping(value={"/addUser"}, method=RequestMethod.POST)
+    public void addUser(User user){
+        userMapper.addUser(user);
+    }
+
+    @RequestMapping(value={"/updateUser"}, method=RequestMethod.POST)
+    public void updateUser(User user){
+        userMapper.updateUser(user);
+    }
+
+    @RequestMapping(value={"/deleteUser"}, method=RequestMethod.POST)
+    public void deleteUser(String id){
+        userMapper.deleteUser(Integer.parseInt(id));
+    }
+}
---
+++ springMybatis/src/main/java/com/spring/mybatis/springMybatis/entity/User.java
@@ -0,0 +1,16 @@
+package com.spring.mybatis.springMybatis.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User{
+    private int id;
+    private String userName; // 用户名
+    private int userAge; // 年龄
+    private String userAddress; 
+
+
+   
+}
