83bb4c9c992a7e7be2c4b01361215c2c85dd1311
Gncute <841895174@qq.com>
2018-01-18 09:24:51 +0800

    update get set

--- springMybatis/src/main/java/com/spring/mybatis/springMybatis/Application.java
+++ springMybatis/src/main/java/com/spring/mybatis/springMybatis/Application.java
@@ -6,9 +6,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
+
+
+
 @SpringBootApplication
 @MapperScan("com.spring.mybatis.springMybatis.*")
 public class Application {
+//	@Bean
+//	public HttpMessageConverters fastJsonHttpMessageConverters(){
+//		
+//		
+//		// 1. 需要先定义一个Convert转换消息的对象
+//		FastJsonHttpMessageConverter fastConverter=new FastJsonHttpMessageConverter();
+//		
+//		// 2.添加fastJson的配置信息，比如：是否需要格式化返回的json数据
+//		FastJsonConfig fastJsonConfig=new FastJsonConfig();
+//	    fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+//	    
+//	    // 3.在conver中添加配置信息
+//		fastConverter.setFastJsonConfig(fastJsonConfig);
+//		
+//		HttpMessageConverter<?> converter =fastConverter;
+//		return new HttpMessageConverters(converter);
+//		
+//	}
+//	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- springMybatis/src/main/java/com/spring/mybatis/springMybatis/entity/User.java
+++ springMybatis/src/main/java/com/spring/mybatis/springMybatis/entity/User.java
@@ -1,15 +1,39 @@
 package com.spring.mybatis.springMybatis.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Setter
+//@Getter
 public class User{
     private int id;
     private String userName; // 用户名
     private int userAge; // 年龄
-    private String userAddress; 
+    private String userAddress;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	} 
 
 
    
