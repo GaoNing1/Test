83b35c21516aed189e82d1dd8c50f392e2200618
Gncute <841895174@qq.com>
2018-01-29 10:24:47 +0800

    springboot redis

---
+++ vic/src/main/java/com/vic/service/IUserService.java
@@ -0,0 +1,15 @@
+package com.vic.service;
+
+import java.util.List;
+
+import com.vic.entity.User;
+
+/**
+ * 
+ * @author gaoning
+ * @desc User Service
+ */
+public interface IUserService {
+
+	public List<User> getAll();
+}
